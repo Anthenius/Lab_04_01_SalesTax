591637b2c15c1a2cd9fe6ae5b719d9832c579256
Anthenius <antheniscenters@gmail.com>
2024-09-28 19:21:41 -0400

    finished project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+       double purchasePrice = 38.42;
+       double salesTax;
+       final double TAX_RATE = .05;
+
+       salesTax = TAX_RATE * purchasePrice;
+
+       System.out.println("The sales tax on a purchase of " + purchasePrice + " is $" + salesTax);
     }
 }
